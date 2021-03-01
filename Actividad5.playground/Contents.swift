d693739ceec310cecb7a19c4309cfde87312c04f
andres <alex-crdnz>
2021-03-01 13:23:06 -0800

    Actividad 5

--- Actividad5.playground/Contents.swift
+++ Actividad5.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+//clases
+class Persona{
+    func salludar(nombre:String) -> String {
+        return "\(nombre) Mucho gusto"
+    }
+    func caminar(pasos:Int) -> String {
+        if (pasos == 1){
+            return "\(pasos) paso caminado"
+        }
+        else{
+            return "\(pasos) pasos caminado"
+        }
+    }
+}
+//estructura
+struct Pantalla{
+    
+    var ancho:Int
+    var alto:Int
+    
+    init(ancho:Int, alto:Int) {
+        self.alto = alto
+        self.ancho = ancho
+    }
+}
