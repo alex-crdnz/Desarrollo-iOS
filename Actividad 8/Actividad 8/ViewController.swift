ec3ddc0d6ce1957734745dbe40148e6ad69e2606
andres <alex-crdnz>
2021-03-24 18:53:32 -0700

    actividad 8

---
+++ Actividad 8/Actividad 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad 8
+//
+//  Created by user183808 on 3/24/21.
+//  Copyright © 2021 user183808. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
