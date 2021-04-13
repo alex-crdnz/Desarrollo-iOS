3f4aa3b2e10c56ef57a1782c3c91df1c647bee43
andres <alex-crdnz>
2021-04-12 21:34:11 -0700

    Actividad 10

---
+++ Actividad 10/Actividad 10/TableVC.swift
@@ -0,0 +1,11 @@
+//
+//  TableVC.swift
+//  Actividad 10
+//
+//  Created by user183808 on 4/12/21.
+//  Copyright © 2021 user183808. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ Actividad 10/Actividad 10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad 10
+//
+//  Created by user183808 on 4/12/21.
+//  Copyright © 2021 user183808. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
